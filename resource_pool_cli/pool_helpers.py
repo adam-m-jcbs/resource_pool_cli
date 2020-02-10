9f55faf9af35a6c37f3f38a9b82cd8b70ee3a2f5
Adam M. Jacobs <ajacobs@pa.msu.edu>
2020-02-09 20:11:30 -0500

    get rid of superfluoous prints

--- resource_pool_cli/pool_helpers.py
+++ resource_pool_cli/pool_helpers.py
@@ -204,7 +204,7 @@ def get_specs(rp_name):
             data = myfile.read()
         facts = json.loads(data)
 
-        click.echo('json facts: {}'.format(facts))
+        #click.echo('json facts: {}'.format(facts))
         # Whether or not a server can be reached, the fact file is generated.
         # Here, we remove facts from servers that cannot be reachced,
         # in order to avoid inaccurate spec counts.
